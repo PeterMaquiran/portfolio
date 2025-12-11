419f14ba5a98419a553f066f72e24c58d2283e87
PeterMaquiran <maquiranpeter29@gmail.com>
2025-12-11 13:46:38 +0100

    static generation

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    // 👇 this enables the standalone output required by your Dockerfile
+  output: 'standalone',
+  images: {
+    unoptimized: true,  // Static export cannot optimize images
+  },
 };
 
 export default nextConfig;
